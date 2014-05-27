9d4264c2186cdb41783f43aeaeec02d18d762afe
Eric Johnson <eric.git@iijo.org>
2014-05-27 15:20:21 -0500

    Checking in changes prior to tagging of version 0.03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 49696e4..bcff82d 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension hi
    
     {{$NEXT}}
    
    +0.03 2014-05-27T20:20:15Z
    +
    +    - Allow digits 0-9 to be included in words.  Previously they indicated a
    +      word boundary.
    +
     0.02 2014-05-27T12:25:31Z
    
         - Add missing dependencies for CPAN Testers

--- lib/String/CamelSnakeKebab.pm
+++ lib/String/CamelSnakeKebab.pm
@@ -12,7 +12,7 @@ use Sub::Exporter -setup => { exports => [qw/
     http_header_case
 /]};
 
-our $VERSION = "0.02";
+our $VERSION = "0.03";
 
 our %UPPER_CASE_HTTP_HEADERS = map { $_ => 1 } 
     qw/ CSP ATT WAP IP HTTP CPU DNT SSL UA TE WWW XSS MD5 /;
